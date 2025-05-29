778d6cc289269176b1a4e60489b57d1070340a88
AnibalUnda <155919904+AnibalUnda@users.noreply.github.com>
2025-05-28 22:53:20 -0500

    Update Proyecto BD - Tarea.sql

--- DATA BASE/Proyecto BD - Tarea.sql
+++ DATA BASE/Proyecto BD - Tarea.sql
@@ -1,14 +1,11 @@
--- Creación de la base de datos
+-- CreaciÃ³n de la base de datos
 
 create database bd_proyecto_indicadores_financieros;
 go
-
 -- Seleccionar la base de datos
 use bd_proyecto_indicadores_financieros;
 go
-
 -- Creamos la tabla analista_responsable
-
 create table analista_responsable (
 id int identity(1,1) primary key,
 cod_analista_origen char(8) unique not null,
@@ -30,7 +27,6 @@ estado varchar(25) not null
 );
 
 -- Creamos la tabla gerente_agencia
-
 create table gerente_agencia (
 id int identity (1,1) primary key,
 agencia_id int not null,
@@ -42,7 +38,6 @@ foreign key (agencia_id) references agencia(id)
 );
 
 -- Creamos la tabla fecha
-
 create table fecha (
 id int identity (1,1) primary key,
 agencia_id int not null,
@@ -51,7 +46,6 @@ foreign key (agencia_id) references agencia(id)
 );
 
 -- Creamos la tabal creditos
-
 create table creditos (
 id int identity (1,1) primary key,
 titular varchar (100) not null,
@@ -61,7 +55,6 @@ estado_credito varchar (100) not null,
 saldo_otorgado decimal (9,2) not null,
 foreign key (agencia_id) references agencia(id)
 );
-
 
 -- Creamos la tabla sistema_fuente
 create table sistema_fuente(
@@ -93,9 +86,23 @@ foreign key (fecha_id) references fecha(id)
 );
 
 -- Creamos la tabla indicador_diario_sf
-
-create table indicador_diario_sf(id int identity(1,1) primary key,indicador_id int not null,agencia_id int not null,valor_meta VARCHAR (255) NOT NULL,valor_real decimal (9,2) NOT NULL,fecha_id int not null,foreign key (indicador_id) references indicador(id),foreign key (agencia_id) references agencia(id),foreign key (fecha_id) references fecha(id));
+create table indicador_diario_sf(
+id int identity(1,1) primary key,
+indicador_id int not null,
+agencia_id int not null,
+valor_meta VARCHAR (255) NOT NULL,
+valor_real decimal (9,2) NOT NULL,
+fecha_id int not null,
+foreign key (indicador_id) references indicador(id),
+foreign key (agencia_id) references agencia(id),
+foreign key (fecha_id) references fecha(id)
+);
 
 -- Creamos la tabla desviaciones_indicador
-
-create table desviaciones_indicador(id INT IDENTITY(1,1) PRIMARY KEY,indicador_diario_sf_id INT NOT NULL,diferencia_absoluta DECIMAL(9,2) NOT NULL,diferencia_porcentual DECIMAL(9,2)NOT NULL,FOREIGN KEY (indicador_diario_sf_id) REFERENCES indicador_diario_sf(id),);
+create table desviaciones_indicador(
+id INT IDENTITY(1,1) PRIMARY KEY,
+indicador_diario_sf_id INT NOT NULL,
+diferencia_absoluta DECIMAL(9,2) NOT NULL,
+diferencia_porcentual DECIMAL(9,2)NOT NULL,
+FOREIGN KEY (indicador_diario_sf_id) REFERENCES indicador_diario_sf(id),
+);
